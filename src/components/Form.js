39544e1cb1dfa4216827fe6f0d22b8102177ef43
八木利生太郎 <yagiryutaro@yagirisrounoAir.AirPort>
2021-03-08 19:53:39 +0900

    first commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,52 +2,52 @@ import React from 'react';
 import './Form.css';
 
 class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isSubmitted: false,
-      };
-    }
-  
-    handleSubmit() {
-      this.setState({isSubmitted: true});
-    }
-  
-    render() {
-      let contactForm;
-      if (this.state.isSubmitted) {
-        contactForm = (
-          <div className='contact-submit-message'>
-            <p>Your message was sent😄<br />Thank you !!</p>
-          </div>
-        );
-      } else {
-        contactForm = (
-          <form action="lxam2cctzwcn@gmail.com" method="post" onSubmit={() => {this.handleSubmit()}}>
-            <div className="contactName">
-                <label>NAME</label>
-                <input type="text" />
-            </div>
-            <div className="contactEmail">
-                <label>EMAIL</label>
-                <input type="text"/>
-            </div>
-            <div className="contactMessage">
-                <label>MESSAGE</label>
-                <textarea />
-            </div>
-            <button type='submit'>SUBMIT</button>
-          </form>
-        );
-      }
-      
-      return (
-        <div className='contact-form'>
-          {contactForm}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitted: false,
+    };
+  }
+
+  handleSubmit() {
+    this.setState({ isSubmitted: true });
+  }
+
+  render() {
+    let contactForm;
+    if (this.state.isSubmitted) {
+      contactForm = (
+        <div className='contact-submit-message'>
+          <p>Your message was sent😸<br />Thank you !!</p>
         </div>
       );
+    } else {
+      contactForm = (
+        <form action="lxam2cctzwcn@gmail.com" method="post" onSubmit={() => { this.handleSubmit() }}>
+          <div className="contactName">
+            <label>NAME</label>
+            <input type="text" />
+          </div>
+          <div className="contactEmail">
+            <label>EMAIL</label>
+            <input type="text" />
+          </div>
+          <div className="contactMessage">
+            <label>MESSAGE</label>
+            <textarea />
+          </div>
+          <button type='submit'>SUBMIT</button>
+        </form>
+      );
     }
+
+    return (
+      <div className='contact-form'>
+        {contactForm}
+      </div>
+    );
   }
-  
+}
+
 
 export default Form;
